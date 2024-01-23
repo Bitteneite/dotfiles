6272bac95326db9f49b38441d9127417247e0658
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-01-23 21:47:13 +0400

    Added prompts

--- 1-install.sh
+++ 1-install.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
-echo 
-echo "Installing packages from applist.txt file..."
-sleep 1
 
-xargs sudo apt install -y < applist.txt
+# Installing packages
+read -p "Do you wish to install packages from applist.txt? (y/n): " answer
 
-echo 
-echo "Installing fonts..."
-sleep 1
-
-stow fonts
-
-echo 
-echo "Changing default shell to Zsh..."
-sleep 1
-chsh -s $(which zsh)
-
+if [ "$answer" = "y" ]; then
+    echo "Installing packages from applist.txt file..."
+    xargs sudo apt install -y < applist.txt
+	echo "Success!"
+fi
 echo
+
+
+# Installing fonts
+read -p "Do you wish to install fonts? (y/n): " answer
+
+if [ "$answer" = "y" ]; then
+	echo "Installing fonts..."
+	stow fonts
+	echo "Success!"
+fi
+echo
+
+
+# Change default shell
+read -p "Do you wish to change the default shell to Zsh? (y/n): " answer
+
+if [ "$answer" = "y" ]; then
+	echo "Changing default shell to Zsh..."
+	chsh -s $(which zsh)
+	echo "Success!"
+fi
+echo
+
+# EXTRAS
 echo "NOTE:"
 echo "- Change your terminal's font to MesloLGS, size 14-16."
 echo "- Please log off to refresh the default shell!"
-sleep 2
--- 2-setup.sh
+++ 2-setup.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
+# Confirmation
+read -p "Do you wish to setup Zsh? (y/n): " answer
+if [ "$answer" = "n" ]; then
+    echo "Aborting setup..."
+    exit 1
+fi
+echo
+
 #Plugins
 # Install Oh-My-Zsh
 echo 
 echo "Installing Oh-My-Zsh..."
-sleep 1
+sleep 2
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # zsh-autosuggestions
 echo 
 echo "Installing zsh-autosuggestions..."
-sleep 1
+sleep 2
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # zsh-syntax-highlighting
 echo 
 echo "Installing zsh-syntax-highlighting..."
-sleep 1
+sleep 2
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Powerlevel10k
 echo 
 echo "Installing Powerlevel10k..."
-sleep 1
+sleep 2
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Deleting old .zshrc
 echo 
 echo "Installing personal .zshrc file..."
-sleep 1
+sleep 2
 stow --adopt zsh
+git restore .
 
 echo
 echo "Open the terminal again to configure Powerlevel10k!"
