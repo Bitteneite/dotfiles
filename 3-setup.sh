05f774793972a4fc69c6465576284ed7722e773b
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-04 19:24:20 +0400

    Update 3-setup.sh

--- 3-setup.sh
+++ 3-setup.sh
@@ -7,20 +7,20 @@ gum confirm "Do you wish to setup Zsh?" && echo "lmao" || exit 1
 # Install Oh-My-Zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+
 # zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+gum spin --spinner points --show-output --title "Installing zsh-autosuggestions..." -- git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+gum spin --spinner points --show-output --title "Installing zsh-syntax-highlighting..." -- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # zsh-you-should-use
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+gum spin --spinner points --show-output --title "Installing zsh-you-should-use..." -- git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+
 # Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+gum spin --spinner points --show-output --title "Installing Powerlevel10k..." -- git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 
 # Deleting old .zshrc
-echo 
-echo "Installing personal .zshrc file..."
-stow --adopt zsh zsh-powerline
-git restore .
-
-echo
-echo "Open the terminal again to configure Powerlevel10k!"
+gum confirm "Install personal .zshrc configuration?" && stow --adopt zsh zsh-powerline &&
+git restore . || echo "Personal configuration not installed."
