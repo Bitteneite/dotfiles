64d81912ccbfe40e6f2e4833b7d331009646a954
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-04 21:45:31 +0400

    Update 3-setup.sh

--- 3-setup.sh
+++ 3-setup.sh
@@ -12,24 +12,23 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 zshautosuggestions(){
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
-gum spin --spinner points --title "Installing zsh-autosuggestions... " -- zshautosuggestions
+gum confirm "Do you wish to install zsh-autosuggestions?" zshautosuggestions
 
 
 zshsyntaxhighlighting(){
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
-gum spin --spinner points --title "Installing zsh-syntax-highlighting... " -- zshsyntaxhighlighting
+gum confirm "Do you wish to install zsh-syntax-highlighting?" zshsyntaxhighlighting
 
-# zsh-you-should-use
 zshyoushoulduse(){
 	git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 }
-gum spin --spinner points --title "Installing zsh-you-should-use... " -- zshyoushoulduse
+gum confirm "Do you wish to install zsh-you-should-use?" zshyoushoulduse
 
 Powerlevel10k(){
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
-gum spin --spinner points --title "Installing Powerlevel10k... " -- Powerlevel10k
+gum confirm "Do you wish to install Powerlevel10k?" Powerlevel10k
 
 
 # Deleting old .zshrc
@@ -37,4 +36,4 @@ zshrc(){
 	stow --adopt zsh
 	git restore .
 }
-gum confirm zshrc || echo "Personal configuration not installed."
+gum confirm "Do you wish to install zshrc?" zshrc || echo "Personal configuration not installed."
