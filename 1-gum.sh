8db8b486373d01ba72e245379452f8003ed7c8d4
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-04 21:36:00 +0400

    Removed the need for Gum for the first script.

--- 1-gum.sh
+++ 1-gum.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
 
-install_ubuntu() {
-	# Debian/Ubuntu Installation for Charm Gum
-	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-	echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
-	sudo apt update && sudo apt install gum
-}
-
-
-#Prompt
-gum confirm "Do you wish to install Charm_ Gum?" && install_ubuntu || echo
+# Debian/Ubuntu Installation for Charm Gum
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
