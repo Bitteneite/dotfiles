b46466ad1a8064383bfcdabfe9552266a6678610
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-02-11 14:03:30 +0400

    Update ASK-w.sh

--- neofetch/.config/neofetch/ASK-w.sh
+++ neofetch/.config/neofetch/ASK-w.sh
@@ -1,19 +1,28 @@
-echo "\e[1;37m"
-echo "       ▄▄▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄     "
-echo "        ▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄▄▄      "
-echo "        ▄▄▄▄ ▄▄▄▄      ▄▄▄▄▄              ▄▄▄▄    ▄▄▄▄       "
-echo "       ▄▄▄▄   ▄▄▄▄        ▄▄▄▄▄▄          ▄▄▄▄▄▄▄▄▄▄▄        "
-echo "       ▄▄▄▄▄▄▄▄▄▄▄           ▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄        "
-echo "      ▄▄▄▄▄▄▄▄▄▄▄▄▄              ▄▄▄▄▄    ▄▄▄▄    ▄▄▄▄       "
-echo "      ▄▄▄▄     ▄▄▄▄    ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄▄▄      "
-echo "     ▄▄▄▄▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄▄▄▄    ▄▄▄▄▄▄   ▄▄▄▄▄▄     "
-echo
-echo "                      Personal Computer                      "
-echo "\e[0m"
-echo "╔═══════════════════════════════════════════════════════════╗"
-echo "║                   ASK Operating System/2                  ║"
-echo "║                        Version 1.00                       ║"
-echo "╚═══════════════════════════════════════════════════════════╝"
-echo       
-echo "      (C) Copyright ASK Corp. 1981. All rights reserved.     "
-echo
+print_center() {
+	
+	local text=$1
+	# Text to be centered:
+	echo "$text" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
+}
+
+
+
+print_center "\e[1;37m"
+print_center "       ▄▄▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄     "
+print_center "        ▄▄▄▄▄▄▄▄▄      ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄▄▄      "
+print_center "        ▄▄▄▄ ▄▄▄▄      ▄▄▄▄▄              ▄▄▄▄    ▄▄▄▄       "
+print_center "       ▄▄▄▄   ▄▄▄▄        ▄▄▄▄▄▄          ▄▄▄▄▄▄▄▄▄▄▄        "
+print_center "       ▄▄▄▄▄▄▄▄▄▄▄           ▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄        "
+print_center "      ▄▄▄▄▄▄▄▄▄▄▄▄▄              ▄▄▄▄▄    ▄▄▄▄    ▄▄▄▄       "
+print_center "      ▄▄▄▄     ▄▄▄▄    ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄▄▄      "
+print_center "     ▄▄▄▄▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄▄▄▄    ▄▄▄▄▄▄   ▄▄▄▄▄▄     "
+print_center
+print_center "                      Personal Computer                      "
+print_center "\e[0m"
+print_center "╔═══════════════════════════════════════════════════════════╗"
+print_center "║                   ASK Operating System/2                  ║"
+print_center "║                        Version 1.00                       ║"
+print_center "╚═══════════════════════════════════════════════════════════╝"
+print_center       
+print_center "      (C) Copyright ASK Corp. 1981. All rights reserved.     "
+print_center
